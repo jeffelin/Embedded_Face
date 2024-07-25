bfbf8576efefb9d828e27485aa4c847b954bbd1b
Jefferson Lin <139834285+jeffelin@users.noreply.github.com>
2024-07-25 10:31:38 -0400

    Delete kinect/pose_estimation - Copy directory

--- kinect/pose_estimation - Copy/READme.md
+++
@@ -1,1 +0,0 @@
-# Kinect Credits: @Anshi Paul, Lukas Dimitroff, Aadhavan Raja Nainar, Shivam Maheshwari team of Kinect
--- kinect/pose_estimation - Copy/test_depth.py
+++
@@ -1,6 +0,0 @@
-import freenect
-
-#Test to get the depth data from the Kinect
-
-depth,_ = freenect.sync_get_depth()
-print(depth)
